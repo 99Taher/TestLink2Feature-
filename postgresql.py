bcd454c264210272fced8b48b959b55d5ce06548
Taherbouhlel <154604826+99Taher@users.noreply.github.com>
2025-07-01 22:25:18 +0100

    Update postgresql.py

--- postgresql.py
+++ postgresql.py
@@ -30,51 +30,51 @@ CREATE TABLE IF NOT EXISTS testcases (
 
 # === Récupération des projets ===
 projects = tlc.getProjects()
-print(f"Projets récupérés : {len(projects)}")
+#print(f"Projets récupérés : {len(projects)}")
 for project in projects:
     project_name = project['name']
     project_id = project['id']
-    print(f"Projet: {project_name} (ID: {project_id})")
+    #print(f"Projet: {project_name} (ID: {project_id})")
 
     suites = tlc.getFirstLevelTestSuitesForTestProject(project_id)
-    print(f"Suites récupérées pour {project_name}: {len(suites)}")
+    #print(f"Suites récupérées pour {project_name}: {len(suites)}")
 
     for suite in suites:
         suite_name = suite['name']
         suite_id = suite['id']
-        print(f"Suite: {suite_name} (ID: {suite_id})")
+        #print(f"Suite: {suite_name} (ID: {suite_id})")
 
         try:
             testcases = tlc.getTestCasesForTestSuite(testsuiteid=suite_id, deep=True)
-            print(f"Test cases récupérées: {len(testcases) if testcases else 0}")
+            #print(f"Test cases récupérées: {len(testcases) if testcases else 0}")
 
             if isinstance(testcases, dict):
-                print("ffffffff")
+                #print("ffffffff")
                 for _, case in testcases.items():
                     case_id = case['id']
                     name = case['name']
                     summary = case.get('summary', '')
-                    print(f"Insertion : {case_id} - {name} - {summary[:30]}")
+                    #print(f"Insertion : {case_id} - {name} - {summary[:30]}")
                     try:
                       cur.execute('''INSERT INTO testcases (id, name, summary, project_name, suite_name)
                         VALUES (%s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING''',
                       (case_id, name, summary, project_name, suite_name))
                     except Exception as e:
-                       print(f"Erreur insertion test case {case_id}: {e}")
+                       #print(f"Erreur insertion test case {case_id}: {e}")
             elif isinstance(testcases, list):
               for case in testcases:
                     case_id = case['id']
                     name = case['name']
                     summary = case.get('summary', '')
-                    print(f"Insertion : {case_id} - {name} - {summary[:30]}")
+                    #print(f"Insertion : {case_id} - {name} - {summary[:30]}")
                     try:
                       cur.execute('''INSERT INTO testcases (id, name, summary, project_name, suite_name)
                         VALUES (%s, %s, %s, %s, %s) ON CONFLICT (id) DO NOTHING''',
                       (case_id, name, summary, project_name, suite_name))
                     except Exception as e:
-                       print(f"Erreur insertion test case {case_id}: {e}")           
+                      # print(f"Erreur insertion test case {case_id}: {e}")           
         except Exception as e:
-            print(f"Erreur pour la suite {suite_name} : {e}")
+            #print(f"Erreur pour la suite {suite_name} : {e}")
 
 
 # === Sauvegarde et fermeture ===
@@ -82,4 +82,4 @@ conn.commit()
 cur.close()
 conn.close()
 
-print(" Base PostgreSQL générée avec succès !")
+#print(" Base PostgreSQL générée avec succès !")
